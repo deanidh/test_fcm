6f6ca09f81a5f3d6d72f13939e2c789b822621a4
deanidh <deanidh1011@gmail.com>
2024-01-29 18:23:34 +0900

    init

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:get/get.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  final fcmToken = await FirebaseMessaging.instance.getToken();
+  debugPrint(fcmToken);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      home: Scaffold(
+        appBar: AppBar(title: Text('hello')),
+        body: const Center(
+            child: ElevatedButton(
+          onPressed: null,
+          child: Text('hi'),
+        )),
+      ),
+    );
+  }
+}
